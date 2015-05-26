3b0e8238e0873c9ad158fc278a8e11bbbac8e796
evanchooly <jlee@antwerkz.com>
2015-05-25 20:10:51 -0400

    sharding tests

--- src/test/java/com/antwerkz/bottlerocket/MongoClusterTest.java
+++ src/test/java/com/antwerkz/bottlerocket/MongoClusterTest.java
@@ -2,19 +2,22 @@ package com.antwerkz.bottlerocket;
 
 import com.jayway.awaitility.Awaitility;
 import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
 import com.mongodb.WriteConcern;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
+
+import static java.util.Arrays.asList;
 
 public class MongoClusterTest {
     @Test
@@ -24,9 +27,10 @@ public class MongoClusterTest {
             return false;
         };
         Awaitility.await()
-            .atMost(30, TimeUnit.SECONDS)
-            .until(waiting);
+                  .atMost(30, TimeUnit.SECONDS)
+                  .until(waiting);
     }
+
     @Test
     public void singleNode() throws InterruptedException, UnknownHostException {
         final SingleNodeBuilder builder = SingleNode.builder();
@@ -70,8 +74,8 @@ public class MongoClusterTest {
             final Document document = new Document("key", "value");
 
             collection
-                  .withWriteConcern(WriteConcern.ACKNOWLEDGED)
-                      .insertOne(document);
+                .withWriteConcern(WriteConcern.ACKNOWLEDGED)
+                .insertOne(document);
 
             final Document first = collection.find().first();
             Assert.assertEquals(document, first);
@@ -87,9 +91,30 @@ public class MongoClusterTest {
     @Test
     public void sharded() {
         final ShardedCluster sharded = ShardedCluster.cluster()
-            .build();
+                                                     .build();
         sharded.clean();
         sharded.start();
+        final MongoClient client = new MongoClient(asList(new ServerAddress("localhost", 30000), new ServerAddress("localhost", 30001),
+                                                          new ServerAddress("localhost", 30002)));
+
+        final ArrayList<Document> list = client.getDatabase("config").getCollection("shards").find().into(new ArrayList<>());
+        Assert.assertEquals(list.size(), 3, "Should find 3 shards");
+        for (Document document : list) {
+            switch (document.getString("_id")) {
+                case "rocket0":
+                    Assert.assertEquals(document.getString("host"), "rocket0/localhost:30003,localhost:30004,localhost:30005");
+                    break;
+                case "rocket1":
+                    Assert.assertEquals(document.getString("host"), "rocket1/localhost:30006,localhost:30007,localhost:30008");
+                    break;
+                case "rocket2":
+                    Assert.assertEquals(document.getString("host"), "rocket2/localhost:30009,localhost:30010,localhost:30011");
+                    break;
+                default:
+                    Assert.fail("found unknown shard member: " + document);
+            }
+        }
+
     }
 
 }
