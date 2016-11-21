3a103d0df707d8d5ccc6dd5283d912815bb8a5a9
evanchooly <jlee@antwerkz.com>
2016-11-20 22:06:38 -0500

    fixes for windows

--- src/main/kotlin/com/antwerkz/bottlerocket/executable/Mongod.kt
+++ src/main/kotlin/com/antwerkz/bottlerocket/executable/Mongod.kt
@@ -2,36 +2,27 @@ package com.antwerkz.bottlerocket.executable
 
 import com.antwerkz.bottlerocket.MongoExecutable
 import com.antwerkz.bottlerocket.MongoManager
-import com.jayway.awaitility.Awaitility
-import com.jayway.awaitility.Duration
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.zeroturnaround.exec.ProcessExecutor
 import org.zeroturnaround.process.Processes
 import java.io.File
 import java.io.FileOutputStream
-import java.util.concurrent.TimeUnit
 
 class Mongod(manager: MongoManager, name: String,
                     port: Int, baseDir: File) : MongoExecutable(manager, name, port, baseDir) {
-    companion object {
-        private val LOG = LoggerFactory.getLogger(Mongod::class.java)
-    }
-
-    override val logger = LoggerFactory.getLogger("Mongod.${port}")
-
+    override val logger: Logger = LoggerFactory.getLogger("Mongod.${port}")
     private val stdOut: FileOutputStream by lazy {
         FileOutputStream(File(baseDir, "mongod.out"))
     }
-
     private val stdErr: FileOutputStream by lazy {
         FileOutputStream(File(baseDir, "mongod.err"))
     }
-
     private val configFile = File(baseDir, "mongod.conf")
 
     fun start(replicaSetName: String? = null) {
         if (process == null || !process?.isAlive!!) {
-            LOG.info("Starting mongod on port ${port}")
+            logger.info("Starting mongod on port ${port}")
             baseDir.mkdirs()
             val configFile = configFile
             manager.writeConfig(configFile, config)
@@ -52,7 +43,7 @@ class Mongod(manager: MongoManager, name: String,
 
             waitForStartUp()
         } else {
-            LOG.warn("start() was called on a running server: ${port}")
+            logger.warn("start() was called on a running server: ${port}")
         }
     }
 }
