88d911c38fc0452e93b290077f69855aaf558f65
evanchooly <jlee@antwerkz.com>
2020-08-01 15:43:43 -0400

    default baseDir to build if it exists or target otherwise

--- src/main/kotlin/com/antwerkz/bottlerocket/BottleRocket.kt
+++ src/main/kotlin/com/antwerkz/bottlerocket/BottleRocket.kt
@@ -8,7 +8,7 @@ object BottleRocket {
     private val LOG = LoggerFactory.getLogger(BottleRocket::class.java)
 
     @JvmField
-    val TEMP_DIR = if(File("buld").exists()) "build" else "target"
+    val TEMP_DIR = System.getProperty("java.io.tmpdir")
 
     @JvmField
     val DEFAULT_NAME = "rocket"
@@ -20,5 +20,5 @@ object BottleRocket {
     val DEFAULT_VERSION = Version.forIntegers(4, 2, 8)
 
     @JvmField
-    val DEFAULT_BASE_DIR = File("$TEMP_DIR/$DEFAULT_NAME")
+    val DEFAULT_BASE_DIR = File(if(File("buld").exists()) "build" else "target", DEFAULT_NAME)
 }
